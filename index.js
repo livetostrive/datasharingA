d228e4f45ef63639565b01f3b9d35ad54479ae7d
livetostrive <44252220+livetostrive@users.noreply.github.com>
2020-10-13 00:14:57 -0400

    React Changes

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"; import ReactDOM from "react-dom"; import Entirecode
-from "./Entirecode.js"; import "./styles.css"; function App() { return (
-<div className="App">
-  <Entirecode />
-</div>
-); } const rootElement = document.getElementById("root"); ReactDOM.render(
-<App />, rootElement);
