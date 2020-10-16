0af5f187056492539d610425e258d9b7945f0cfc
livetostrive <44252220+livetostrive@users.noreply.github.com>
2020-10-16 19:32:35 -0400

    adding bootstrap features

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import React from "react"; import ReactDOM from "react-dom"; import Entirecode
-from "./Entirecode.js"; import "./styles.css"; function App() { return (
+import React from "react";
+import ReactDOM from "react-dom";
+import Entirecode from "./Entirecode.js";
+import 'bootstrap/dist/css/bootstrap.css';
+import "./styles.css"; function App() {
+  return (
 <div className="App">
   <Entirecode />
 </div>
